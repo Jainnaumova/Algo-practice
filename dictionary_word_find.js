ab4357474263423375fec496080980544c463504
Evgeniya Naumova <jainnaumova@gmail.com>
2019-08-20 16:39:48 -0400

    add new solution for dictionary word finder

--- dictionary_word_find.js
+++ dictionary_word_find.js
@@ -29,9 +29,33 @@ const findDefinitionOf = (dict, word) => {
   return def.slice(word.length + 3);
 }
 
+// Big O (n)
+
+// Case if dictionary is ordered !
+
+const findDefinitionOf = (dict, word) => {
+// Initialize left, right and middle indices
+  let left = 0;
+  let right = dict.length - 1;
+  let index;
+  while (index !== left && index !== right) {
+    index = Math.floor((right + left) / 2); // middle of dictionary arbitrary
+    if (dict[index].startsWith(word + ' - ')) return dict[index].split('- ')[1];
+
+// continue searching in the left part of the dictionary
+    if (word < dict[index]) {
+      right = index - 1;
+    } else {
+
+// continue searching in the rigth part of the dictionary
+      left = index + 1;
+    }
+  }
+}
+
+// Big O (log n)
+
 // definitionOf('be', dictionary); // should return 'Exist'
 // definitionOf('that', dictionary); // should return 'Used to identify a specific person or thing observed or heard by the speaker'
 // definitionOf('to', dictionary); // should return 'Expressing motion in the direction of (a particular location)'
 // definitionOf('wizbing', dictionary); // should return undefined
-
-// Big O (n)
