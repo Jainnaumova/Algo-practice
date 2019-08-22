f2c543bf431a7ded22938a82f859f48a7b4dc9fa
Evgeniya Naumova <jainnaumova@gmail.com>
2019-08-22 01:04:20 -0400

    add HashMap solution to count the numbers of pairs that have difference k

--- pairs_with_difference.js
+++ pairs_with_difference.js
@@ -26,6 +26,33 @@ const findPairs = (arr, k) => {
   return count;
 }
 
+// using HashMap
+
+const findPairs = (arr, k) => {
+  if (arr.length < 2) return -1;
+
+  let mySet = new Set();
+  for (let i = 0; i < arr.length; i++) {  // O (n)
+// Avoid duplicates in object
+    if (!mySet.has(arr[i])) {
+      mySet.add(arr[i]);
+    }
+  }
+
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {  // O (n)
+    if(mySet.has(arr[i] - k)) {
+      count++;
+    }
+    if(mySet.has(arr[i] + k)) {
+      count++;
+    }
+// Avoid count duplicate pairs
+    mySet.delete(arr[i]);
+  }
+  return count;
+}
+
 //  Big O (n)
 
 findPairs([1,7,5,9,2,12,3], 2) // return 4
