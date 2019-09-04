33ff778e7653aafb136bc2a00606b4d740cfd6a0
Evgeniya Naumova <jainnaumova@gmail.com>
2019-09-04 00:50:39 -0400

    get intersection of two arrays

--- big-o/various_functions.js
+++ big-o/various_functions.js
@@ -13,3 +13,19 @@ const shuffleArray = (arr) => {
 
 shuffleArray([1,6,2,3,4,5,5,6])
 // runtime O(n)
+
+// get intersection of two arrays
+const getIntersection = (arr1, arr2) => {
+  let intersectedArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        intersectedArr.push(arr2[j])
+      }
+    }
+  }
+  return intersectedArr;
+}
+
+getIntersection([1,2,3,4,5], [2,3,4,8,9,0]) // return [2,3,4]
+// runtime O(n*m)
