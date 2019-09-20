2ba25ad70fd9b48bdaa3b14d1ddab166dc701622
Evgeniya Naumova <jainnaumova@gmail.com>
2019-09-20 00:15:15 -0400

    find the second largest value of BST

--- data-structure/8-binary-search-tree.js
+++ data-structure/8-binary-search-tree.js
@@ -63,3 +63,23 @@ class BinarySearch {
 }
 
 // time complexity O(logn), space complexity O(n);
+
+function findSecondLargest(root) {
+  // If tree is null or is single node only, return null (no second largest)
+  if (root==null || (root.left==null && root.right==null)) return null;
+  parent = null, child = root;
+  // find the right most child
+  while (child.right!=null) {
+      parent = child;
+      child = child.right;
+   }
+   // if the right most child has no left child, then it's parent is second largest
+   if (child.left==null) return parent.value;
+   // otherwise, return left child's rightmost child as second largest
+   child = child.left;
+   while (child.right!=null) {
+     child = child.right;
+   }
+   return child.value;
+
+}
