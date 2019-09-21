4470b631d89ae6d0eb25cef98d299fe5be6a3217
Evgeniya Naumova <jainnaumova@gmail.com>
2019-09-20 20:43:34 -0400

    fix merge sort

--- merge_sort.js
+++ merge_sort.js
@@ -1,23 +1,21 @@
 const mergeSort = arr => {
   if (arr.length < 2) return arr;
-  let middle = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0, middle));
-  let right = mergeSort(arr.slice(middle));
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
   return merge(left, right);
 }
 
 const merge = (arrLeft, arrRight) => {
-  let result = [];
-  while (arrLeft.length && arrRight.length) {
-    let el;
-    if (arrLeft[0] < arrRight[0]) {
-      el = arrLeft.shift();
+  let result = []
+  while (arrLeft.length || arrRight.length) {
+    if (arrLeft[0] <= arrRight[0] || !arrRight.length) {
+      result.push(arrLeft.shift());
     } else {
-      el = arrRight.shift();
+      result.push(arrRight.shift());
     }
-    result.push(el);
   }
-  return result.concat(arrLeft.length ? arrLeft[0] : arrRight[0]);
+  return result;
 }
 
 mergeSort([2,5,6,10,11,3,15]) // return [ 2, 3, 5, 6, 10, 11, 15 ]  n*log(n)
